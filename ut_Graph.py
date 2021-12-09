97136fc7464819c1b5a09aa8df20349835d6624b
Aleksander <ale-k-san-der@hotmail.com>
2021-12-09 10:19:37 +0000

    Add files via upload

--- ut_Graph.py
+++ ut_Graph.py
@@ -4,30 +4,39 @@ from Graph import Graph
 
 class ut_Graph(unittest.TestCase):
     def test_addNeighborhood(self):
-        graphBase = Graph(7, [1, 2, 3], [10, 20, 30])
+        graphBase = Graph(7, [1, 2, 3], [10.0, 20.0, 30.0])
 
         graphTest = Graph(7)
-        graphTest.addNeighbour(1, 10)
-        graphTest.addNeighbour(2, 20)
-        graphTest.addNeighbour(3, 30)
+        graphTest.addNeighbour(1, 10.0)
+        graphTest.addNeighbour(2, 20.0)
+        graphTest.addNeighbour(3, 30.0)
 
         self.assertEqual(graphBase, graphTest)
 
     def test_removeNeighborhood(self):
-        graphBase = Graph(7, [1, 3], [10, 30])
+        graphBase = Graph(7, [1, 3], [10.0, 30.0])
 
-        graphTest = Graph(7, [1, 2, 3], [10, 20, 30])
+        graphTest = Graph(7, [1, 2, 3], [10.0, 20.0, 30.0])
         graphTest.removeNeighbour(2)
 
         self.assertEqual(graphBase, graphTest)
 
     def test_incrementAgeNeighborhood(self):
-        graphBase = Graph(7, [1, 2, 3], [11, 21, 31])
+        graphBase = Graph(7, [1, 2, 3], [11.0, 21.0, 31.0])
 
-        graphTest = Graph(7, [1, 2, 3], [10, 20, 30])
-        graphTest.incrementAgeNeighborhood(1)
+        graphTest = Graph(7, [1, 2, 3], [10.0, 20.0, 30.0])
+        graphTest.incrementAgeNeighborhood(1.0)
 
         self.assertEqual(graphBase, graphTest)
+
+    def test_incrementAgeNeighbour(self):
+        graphBase = Graph(7, [1, 2, 3], [10.0, 21.0, 30.0])
+
+        graphTest = Graph(7, [1, 2, 3], [10.0, 20.0, 30.0])
+        graphTest.incrementAgeNeighbour(2, 1.0)
+
+        self.assertEqual(graphBase, graphTest)
+
 
     def test_incrementAgeNeighborhood_when_graph_have_not_neighborhood(self):
         graphBase = Graph(7, [], [])
@@ -38,18 +47,21 @@ class ut_Graph(unittest.TestCase):
         self.assertEqual(graphBase, graphTest)
 
     def test_pruneGraph(self):
-        graphBase = Graph(7, [1, 2, 3, 4, 5], [25, 20, 30, 25, 20])
+        graphBase_30 = Graph(7, [1, 2, 4, 5], [25.0, 20.0, 25.0, 20.0])
+        graphBase_25 = Graph(7, [2, 5], [20.0, 20.0])
+        graphBase_20 = Graph(7)
 
-        graphTest_30 = Graph(7, [1, 2, 4, 5], [25, 20, 25, 20])
-        graphTest_25 = Graph(7, [2, 5], [20, 20])
+        graphTest = Graph(7, [1, 2, 3, 4, 5], [25.0, 20.0, 30.0, 25.0, 20.0])
 
-        graphBase.pruneGraph(30)
+        graphTest.pruneGraph(30.0)
+        self.assertEqual(graphTest, graphBase_30)
 
-        self.assertEqual(graphBase, graphTest_30)
+        graphTest.pruneGraph(25.0)
+        self.assertEqual(graphTest, graphBase_25)
 
-        graphBase.pruneGraph(25)
+        graphTest.pruneGraph(20.0)
+        self.assertEqual(graphTest, graphBase_20)
 
-        self.assertEqual(graphBase, graphTest_25)
 
 if __name__ == '__main__':
     unittest.main()
--- ut_GrowingNeuralGas.py
+++ ut_GrowingNeuralGas.py
@@ -19,11 +19,11 @@ class ut_GrowingNeuralGas(unittest.TestCase):
     def test_pruneA(self):
         aBase = tf.constant([[0., 0., 0.], [0.5, 0.1, 0.], [0.1, 0.5, 0.5]], dtype=tf.float32)
         nBase = []
-        nBase.append(Graph(0, [2, 4], [71, 31]))
-        nBase.append(Graph(1, [0, 4], [21, 41]))
-        nBase.append(Graph(2, [0, 2], [11, 32]))
+        nBase.append(Graph(0, [1, 2], [71, 31]))
+        nBase.append(Graph(1, [0, 2], [21, 41]))
+        nBase.append(Graph(2, [0, 1], [11, 32]))
 
-        aTest = tf.constant([[0., 0., 0.], [0., 1., 0.], [0.5, 0.1, 0.], [2., 3., 0.5], [0.1, 0.5, 0.5]], dtype=tf.float32)
+        aTest = tf.Variable([[0., 0., 0.], [0., 1., 0.], [0.5, 0.1, 0.], [2., 3., 0.5], [0.1, 0.5, 0.5]], dtype=tf.float32)
         nTest = []
         nTest.append(Graph(0, [2, 4], [71, 31]))
         nTest.append(Graph(1))
@@ -31,14 +31,36 @@ class ut_GrowingNeuralGas(unittest.TestCase):
         nTest.append(Graph(3))
         nTest.append(Graph(4, [0, 2], [11, 32]))
 
-        growingNeuralGas = GrowingNeuralGas()
-        growingNeuralGas.A = aTest
-        growingNeuralGas.N = nTest
+        growingNeuralGasTest = GrowingNeuralGas()
+        growingNeuralGasTest.A = aTest
+        growingNeuralGasTest.N = nTest
 
-        growingNeuralGas.pruneA()
+        growingNeuralGasTest.pruneA()
 
-        self.assertTrue(tf.math.reduce_all(aBase == growingNeuralGas.A))
-        for graphBase, graphTest in zip(nBase, growingNeuralGas.N):
+        self.assertTrue(tf.math.reduce_all(aBase == growingNeuralGasTest.A))
+        for graphBase, graphTest in zip(nBase, growingNeuralGasTest.N):
+            self.assertEqual(graphBase, graphTest)
+
+    def test_getGraphConnectedComponents(self):
+        connectedComponentBase_0 = [Graph(0, [4, 9], [0.0, 0.0]), Graph(4, [0, 8, 11], [0.0, 0.0, 0.0]), Graph(8, [4, 9], [0.0, 0.0]), Graph(9, [0, 8], [0.0, 0.0]),
+                                    Graph(11, [4], [0.0])]
+        connectedComponentBase_1 = [Graph(1, [6], [0.0]), Graph(2, [5, 6], [0.0, 0.0]), Graph(5, [2, 6], [0.0, 0.0]), Graph(6, [1, 2, 5], [0.0, 0.0, 0.0])]
+        connectedComponentBase_2 = [Graph(3, [7, 10], [0.0, 0.0]), Graph(7, [3, 10], [0.0, 0.0]), Graph(10, [3, 7], [0.0, 0.0])]
+
+        nTest = [Graph(0, [4, 9], [0.0, 0.0]), Graph(1, [6], [0.0]), Graph(2, [5, 6], [0.0, 0.0]), Graph(3, [7, 10], [0.0, 0.0]), Graph(4, [0, 8, 11], [0.0, 0.0, 0.0]),
+                 Graph(5, [2, 6], [0.0, 0.0]), Graph(6, [1, 2, 5], [0.0, 0.0, 0.0]), Graph(7, [3, 10], [0.0, 0.0]), Graph(8, [4, 9], [0.0, 0.0]), Graph(9, [0, 8], [0.0, 0.0]),
+                 Graph(10, [3, 7], [0.0, 0.0]), Graph(11, [4], [0.0])]
+
+        growingNeuralGasTest = GrowingNeuralGas()
+        growingNeuralGasTest.N = nTest
+
+        numberConnectedComponent, connectedComponents = growingNeuralGasTest.getGraphConnectedComponents()
+        self.assertTrue(numberConnectedComponent == 3)
+        for graphBase, graphTest in zip(connectedComponentBase_0, connectedComponents[0]):
+            self.assertEqual(graphBase, graphTest)
+        for graphBase, graphTest in zip(connectedComponentBase_1, connectedComponents[1]):
+            self.assertEqual(graphBase, graphTest)
+        for graphBase, graphTest in zip(connectedComponentBase_2, connectedComponents[2]):
             self.assertEqual(graphBase, graphTest)
 
 
