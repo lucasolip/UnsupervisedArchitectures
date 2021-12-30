11ac0e38d769ab2bd2c8c5c80e2a13b4f9dd46e3
Aleksander <57395146+AleksanderEZ@users.noreply.github.com>
2021-12-30 20:58:27 +0000

    ea

--- mainCountryData.py
+++ mainCountryData.py
@@ -1,8 +1,9 @@
 import csvController
 from GrowingNeuralGas import GrowingNeuralGas
+from GrowingNeuralGasPlotter import GrowingNeuralGasPlotter
 import matplotlib as plt
 
-epochs = 10
+epochs = 20
 
 def test():
 
@@ -10,16 +11,32 @@ def test():
     countryData = csvController.load_data("Datos_Paises.csv")
     csvController.removeColumns(countryData, ["Country", "Region"])
     countryData = csvController.replaceNaNWithMedian(countryData)
-
+#
     #Principal component analysis
     countryData = csvController.pcaByVarianceRetention(countryData, 0.95)
-
+#
     #Transform to tensor
     countryData = csvController.fromDataFrameToTensor(countryData)
-
+#
     growingNeuralGas = GrowingNeuralGas(eta=25)
-    growingNeuralGas.fit(countryData, epochs)
+    #growingNeuralGas.fit(countryData, epochs)
+    #print(growingNeuralGas.countClusters())
+    #print(growingNeuralGas.A.shape)
+#
+    #GrowingNeuralGasPlotter.plotNetworkStructure3D(growingNeuralGas.A, countryData, growingNeuralGas.getEdges(), title="Estructura de los datos paises.\n"
+    #                                                                                                                   "PCA aplicado para obtener 3 dimensiones\n"
+    #                                                                                                                    "Retención del 50% de la información")
+    #GrowingNeuralGasPlotter.show()
+
+    growingNeuralGas.loadModel("model.h")
     print(growingNeuralGas.countClusters())
-    print(growingNeuralGas.A.shape)
+    print(growingNeuralGas.predict(countryData[0]))
+    print(growingNeuralGas.predict(countryData[1]))
+    print(growingNeuralGas.predict(countryData[2]))
+    print(growingNeuralGas.predict(countryData[3]))
+    print(growingNeuralGas.predict(countryData[4]))
+    print(growingNeuralGas.predict(countryData[5]))
+    print(growingNeuralGas.predict(countryData[6]))
+    print(growingNeuralGas.predict(countryData[7]))
 
 test()
